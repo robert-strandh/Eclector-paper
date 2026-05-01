7654e5ff5fa34a6e7458c726947dd3f8b8838d1d
Jan Moringen <jan.moringen@posteo.de>
2026-05-01 19:59:34 +0200

    Include ECL benchmark result by building and using the master version

--- benchmark/benchmark.lisp
+++ benchmark/benchmark.lisp
@@ -1,5 +1,26 @@
 (setf *default-pathname-defaults* #P"~/Projects/\\[2026\\]paper-els-eclector/benchmark/")
 
+(unless (probe-file "/tmp/ecl-test/bin/ecl")
+  (unless (probe-file "/tmp/ecl/configure")
+    (uiop:run-program '("/usr/bin/git"
+                        "clone"
+                        "https://gitlab.common-lisp.net/ecl/ecl")
+                      :directory "/tmp/"))
+  (uiop:run-program '("/usr/bin/git"
+                      "checkout" "3d83b007947590136278b4af2e19b3912873f21d")
+                    :output    *standard-output*
+                    :error     *error-output*
+                    :directory "/tmp/ecl/")
+  (uiop:run-program '("/tmp/ecl/configure"
+                      "--prefix" "/tmp/ecl-test")
+                    :output    *standard-output*
+                    :error     *error-output*
+                    :directory "/tmp/ecl/")
+  (uiop:run-program '("/usr/bin/make" "-j" "2" "install")
+                    :output    *standard-output*
+                    :error     *error-output*
+                    :directory "/tmp/ecl/"))
+
 (uiop:run-program
  '("/usr/bin/sbcl"
    "--noinform"
@@ -18,11 +39,12 @@
  :error     *error-output*
  :directory *default-pathname-defaults*)
 
-#+no (uiop:run-program
- '("/usr/bin/ecl"
+(uiop:run-program
+ '("/tmp/ecl-test/bin/ecl"
    "-q"
    "--load" "/home/jmoringe/.local/share/common-lisp/quicklisp/setup.lisp"
-   "--load" "run.lisp")
+   "--load" "run.lisp"
+   "--eval" "(quit)")
  :output    *standard-output*
  :error     *error-output*
  :directory *default-pathname-defaults*)
